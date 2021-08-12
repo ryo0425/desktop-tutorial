f8a7f241ed012c8082fee710130992d5363dbc02
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-12 23:19:20 +0900

    PHP2章課題（配列、連想配列）

---
+++ 2-7/index.php
@@ -0,0 +1,10 @@
+<?php
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑"]; 
+var_dump($colors);
+
+echo '<br>';
+
+$colors = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"]; 
+var_dump($colors);
+
+?>
