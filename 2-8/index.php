d8e7b1dd6c1b6c242e1362670db06331f0722a2a
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-14 20:41:01 +0900

    PHP2章 foreach

---
+++ 2-8/index.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["appleといったら" =>"りんご", "orangeといったら" => "みかん", "peachといったら" => "もも"];
+
+foreach ($fruits as $key => $value) {
+    echo $key;
+    echo $value;
+    echo '<br>';
+}
+
+?>
