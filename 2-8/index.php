a41ec377a13d8f2898860ef073ffe04d6a1e4593
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-16 22:18:08 +0900

    PHP2章 foreach

--- 2-8/index.php
+++ 2-8/index.php
@@ -1,8 +1,9 @@
 <?php
-$fruits = ["appleといったら" =>"りんご", "orangeといったら" => "みかん", "peachといったら" => "もも"];
+$fruits = ["apple" =>"りんご", "orange" => "みかん", "peach" => "もも"];
 
 foreach ($fruits as $key => $value) {
     echo $key;
+    echo "といったら";
     echo $value;
     echo '<br>';
 }
