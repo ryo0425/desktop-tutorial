c06d4fe292d10d7d24d22fc94bdec0fc9e3a75f9
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-21 16:08:47 +0900

    PHP2章　FizzBuzz

--- 2-9/index.php
+++ 2-9/index.php
@@ -3,14 +3,14 @@
 for ($i = 1; $i <= 100; $i++) {
 echo '<br>';
 
-if($i % 3 == 0){
-print "Fizz";
+if($i % 3 == 0 && $i % 5 == 0){
+print "FizzBuzz";
 
 }else if($i % 5 == 0){
 print "Buzz";
 
-}else if($i % 3 == 0 && $i % 5 == 0){
-print "FizzBuzz";
+}else if($i % 3 == 0){
+    print "Fizz";
 
 }else{
 print $i;
