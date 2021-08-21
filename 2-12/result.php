4c82100dd625b2a4ee875f4e78864f959f4dee67
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-21 22:48:57 +0900

    PHP2章　いろいろなフォームの入力欄

---
+++ 2-12/result.php
@@ -0,0 +1,9 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $prize = $_POST['prize'];
+    $number = $_POST['number'];
+    ?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望景品<?php echo $prize; ?></p>
+    <p>個数：<?php echo $number; ?></p>
+    <p></p>
