e62eb1da8f3d6d0bd4b312eaf4afed151bb1839f
ryo0425 <86556554+ryo0425@users.noreply.github.com>
2021-08-21 19:55:56 +0900

    PHP2章　フォームに入力した内容を取得する

---
+++ 2-11/index.php
@@ -0,0 +1,9 @@
+<form action="result.php" method="get">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="mailaddress" name="mailaddress" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$mailaddress = $_POST['mailaddress'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $mailaddress; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
